fa4b971ad8732a55b3eebe6399aefc1eda779054
Sai-prava <saipravakhilar88@gmail.com>
2024-12-09 18:04:48 +0530

    pos msr

--- app/Http/Controllers/Admin/DsrController.php
+++ app/Http/Controllers/Admin/DsrController.php
@@ -94,7 +94,6 @@ class DsrController extends Controller
             DB::raw('SUM(billing_amount) as total_billing_amount')
         )->whereNotNull(['transaction_date', 'user_id'])
             ->with(['user', 'getPos'])
-            ->whereNotNull('transaction_date')
             ->orderBy('id', 'desc')
             ->groupBy(['user_id', 'mobilenumber',  DB::raw('DATE_FORMAT(transaction_date, "%Y-%m")')]);
 
@@ -114,22 +113,37 @@ class DsrController extends Controller
             $selectedMonth = $request->month;
             $query->whereRaw('DATE_FORMAT(transaction_date, "%Y-%m") = ?', [$selectedMonth]);
         }
-        $monthlySales = $query->orderBy('id', 'desc')->simplePaginate(15)->through(function ($item) {
-            // Log::info($item->user_id);
+        $monthlySales = $query->orderBy('id', 'desc')->simplePaginate(15)->through(function ($item) use (&$billing_amount) {
+            Log::info($item->user_id);
             $billing_amount = 0;
+            $month_amount = 0;
             $check_sponser = Sponsor::with('user')->where('sponsor_id', $item->user_id)->get();
             if (!$check_sponser->isEmpty()) {
-                $check_sponser->map(function ($items) use (&$billing_amount) {
-                    $billing_amount += (Wallet::where('user_id', $items->user_id)->sum('billing_amount'));
-                    // dd($billing_amount);
+                $check_sponser->map(function ($items) use (&$billing_amount ,&$month_amount) {
+                    $monthlyBilling = Wallet::select(
+                        DB::raw('DATE_FORMAT(transaction_date, "%Y-%m") as transaction_month'),
+                        DB::raw('SUM(billing_amount) as total_billing_amount')
+                    )
+                        ->where('user_id', $items->user_id)
+                        ->groupBy(DB::raw('DATE_FORMAT(transaction_date, "%Y-%m")'))
+                        ->get();
+
+                    // Sum the total_billing_amount for all months
+                    foreach ($monthlyBilling as $monthData) {
+                        $month_amount = $monthData->total_billing_amount;
+                        $billing_amount += $monthData->total_billing_amount;
+                    }
                 });
             }
             $item->sponsor_expenditure = $billing_amount;
+            // $item->sponsor_id = $check_sponser->id??"N/A";
+            $billing_amount = 0;
+            Log::info($item->toArray());
             return $item;
         });
 
         // $monthlySales = $query->orderBy('id', 'desc')->simplePaginate(15);
-        $monthlySales->appends($request->only(['search', 'month', 'sponsor_id']));
+        $monthlySales->appends($request->only(['search', 'month',]));
 
         return view('admin.msr.index', compact('pos', 'monthlySales'));
     }
